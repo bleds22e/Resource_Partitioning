f4a32305159fa3f4d897feb21bf5a915ae740b6a
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2017-03-15 10:15:43 -0400

    updated scripts so they all work together

--- Scripts/trnL_data_prep.R
+++ Scripts/trnL_data_prep.R
@@ -7,36 +7,37 @@
 
 library(dplyr)
 library(stringr)
+library(ggplot2)
 
 ########################
 # LOAD FILES
 
-data <- read.csv("C:/Users/ellen.bledsoe/Desktop/Git/Metagenomics/Plants/trnL_fecal_samples.csv", header = TRUE)
-
-samples <- read.csv("C:/Users/ellen.bledsoe/Desktop/Git/Metagenomics/CollectionData/fecal_sample_collection.csv", header = TRUE)
+trnL <- read.csv("./Plants/trnL_fecal_samples.csv", header = TRUE)
+samples <- read.csv("./CollectionData/fecal_sample_collection.csv", header = TRUE)
 
 ########################
 # CLEAN DATA
 
-data <- data[-(c(452:453)),]
+trnL <- trnL[-(c(452:453)),]
 
 # taxonomy dataframe
 
-taxa <- select(data, OTU_ID, ConsensusLineage) %>% 
-  rename(OTU.ID = OTU_ID)
+taxa_trnL <- select(trnL, OTU.ID, ConsensusLineage)
 
 for(this_level in c('d','k','p','c','o','f','g','s')){
   # separate taxa into columns
-  step_one=sapply(strsplit(as.character(taxa$ConsensusLineage), paste0(this_level,'__')), '[', 2)
+  step_one=sapply(strsplit(as.character(taxa_trnL$ConsensusLineage), paste0(this_level,'__')), '[', 2)
   step_two=sapply(strsplit(step_one, ';'), '[', 1)
-  taxa[,this_level]=step_two
+  taxa_trnL[,this_level]=step_two
 }
+
+taxa_trnL <- rename(taxa_trnL, Family = o, Genus = g, Species = s)
 
 # reads dataframe
 
-reads <- data[,c(1,8:54)] %>% rename(OTU.ID = ï...OTU.ID)
-reads <- tidyr::gather(reads, "Sample", "Proportion", 2:48) %>% 
-  filter(Proportion != 0)
+reads <- trnL[,c(1,8:54)]
+reads <- tidyr::gather(reads, "Sample", "Reads", 2:48) %>% 
+  filter(Reads != 0)
 
 # samples dataframe
 
